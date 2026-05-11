4aab7dd6b236f67bd4c0eacfb580017bda47d158
VireArcanist <virelyn@outlook.de>
2026-05-11 09:22:42 +0200

    Add completeTask and deleteTask methods

--- task.java
+++ task.java
@@ -1,8 +1,10 @@
+// Task.java
+// Class to represent a task with an ID, title, and completion status
 public class Task {
     int id;
     String title;
     boolean isCompleted;
-
+    // Constructor to initialize a new task with an ID and title. The completion status is set to false by default.
     public Task(int id, String title) {
         this.id = id;
         this.title = title;
