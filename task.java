3f48b87cd9b7ba21ade1d8e060763c44b6656a11
VireArcanist <virelyn@outlook.de>
2026-05-11 08:50:10 +0200

    create class task

---
+++ task.java
@@ -0,0 +1,10 @@
+public class task {
+    int id;
+    String title;
+    boolean isCompleted = false;
+
+    public task(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+}
